893645585b69f62c604d9efed7746a699b74aab9
dbuendia <buendia.barcelo@gmail.com>
2023-01-18 11:58:55 +0100

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import "./App.css";
+
+export function App() {
+  return (
+    <article>
+      <header>
+        <img src="https://unavatar.io/ayumi" alt="User avatar" />
+        <div>
+          <strong>Ayumi Tanaka</strong>
+          <span>@ayumuitanaka</span>
+        </div>
+      </header>
+
+      <aside>
+        <button>Seguir</button>
+      </aside>
+    </article>
+  );
+}
